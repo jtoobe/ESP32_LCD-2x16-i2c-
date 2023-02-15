101c3b893e3fd657d87379efad298d7555cae783
jtoobe <125473983+jtoobe@users.noreply.github.com>
2023-02-15 14:15:30 -0300

    Create ESP32_Scan_I2C.ino
    Al compilar da un error con la librería standard pero funciona
    
    Escaneamos los puertos I2C para saber la dirección HEXA del display

---
+++ ESP32_Scan_I2C.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+ 
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Escaneando puertos.");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C dispositivo encontrado en 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Error en direccion 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No se encontraron dispositivos\n");
+  }
+  else {
+    Serial.println("Terminado\n");
+  }
+  delay(5000);          
+}
